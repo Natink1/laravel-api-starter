f6abff74b54dfcdbb936ddbe4f307c92fae8b7d9
Natink1 <natink4825@gmail.com>
2026-05-20 17:42:22 +0300

    Phone number

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,12 +50,14 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            'phonenumber' => 'required|digits:10',
             'password' => 'required|string|min:6',
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phonenumber' => $request->phonenumber,
             'password' => Hash::make($request->password),
         ]);
 
